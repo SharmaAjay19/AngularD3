25dd1b129a45673703a78c5ea93f1edf128ff858
Ajay Sharma <ajsharm@microsoft.com>
2018-09-16 15:20:27 +0530

    Added draggability in axes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {AngularDraggableModule} from 'angular2-draggable';
 import { AppComponent } from './app.component';
 
 import {ScatterPlot} from './visuals/scatterplot/scatterplot.component';
@@ -15,7 +15,8 @@ import {ScatterPlot} from './visuals/scatterplot/scatterplot.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularDraggableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
